da9fc04bc8177b3adf7660e8cda6c524ad446489
Farid Joubbi <farid@joubbi.se>
2022-11-12 16:34:32 +0100

    Update to RHEL 8.7.

--- rhel8-hardened/build.sh
+++ rhel8-hardened/build.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
-PACKER_CACHE_DIR=../packer_cache/ /usr/bin/packer build rhel8-hardened.json 
+packer init rhel8-hardened.pkr.hcl
+PACKER_CACHE_DIR=../packer_cache/ /usr/bin/packer build rhel8-hardened.pkr.hcl 
